5f241797767a1ac6b742baa24bcea4c25227c60a
youtonghy <hi@youtonghy.com>
2026-09-04 11:15:19 +0800

    test: gate Unix-only command import

--- src/server/v2.rs
+++ src/server/v2.rs
@@ -2273,6 +2273,7 @@ struct WsPermissionRequest {
 mod tests {
     use std::collections::BTreeMap;
     use std::fs;
+    #[cfg(unix)]
     use std::process::Command as StdCommand;
     use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
